f82001243a32e0036e919e2985947bbfb8e8d72a
Bikash Roshan <bikash.roshan@stackmybiz.com>
2021-09-10 16:36:05 +0530

    commit all asiignment

---
+++ assignment1.java
@@ -0,0 +1,17 @@
+public class assignment1 {
+    void sum(){
+
+    int num1=74;
+    int num2=36;
+    System.out.printf("%d" ,num1+num2).println();
+
+    }
+
+    public static void main(String[] args) {
+        assignment1 result = new assignment1();
+        result.sum();
+
+        
+    }
+    
+}
---
+++ assignment2.java
@@ -0,0 +1,18 @@
+public class assignment2 {
+    void divide(){
+
+    int num1=50;
+    int num2=3;
+    System.out.printf("%d",num1/num2).println();
+
+    }
+
+    public static void main(String[] args) {
+        assignment2 result = new assignment2();
+        result.divide();
+
+        
+    }
+
+    
+}
