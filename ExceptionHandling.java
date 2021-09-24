57e21021dbe7354e3050def406537d2de7ea6a5a
Bikash Roshan <bikash.roshan@stackmybiz.com>
2021-09-24 18:05:24 +0530

    commit of exercise on 24

---
+++ ExceptionHandling.java
@@ -0,0 +1,24 @@
+public class ExceptionHandling {
+    int a,b,c;
+
+    public void method1() {
+        try{
+        int [] arr = {1, 4, 7, 3};
+       int num = arr[4];
+        System.out.println(num);
+        } catch(ArithmeticException ex){
+            System.out.println("this is wrong");
+        } 
+        catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("index is wrong on method 1");
+        }
+    }
+    public void method2() {
+        System.out.println("this is method 2");
+        method1();
+    }
+    public static void main(String[] args) {
+        ExceptionHandling except = new ExceptionHandling();
+        except.method1();
+    }
+}
