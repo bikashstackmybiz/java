2a42a965e38db9f8f27852a0772a87bb9c947d9a
bikashstackmybiz <90446857+bikashstackmybiz@users.noreply.github.com>
2021-09-27 10:45:06 +0530

    Update ExceptionExample.java

--- ExceptionExample.java
+++ ExceptionExample.java
@@ -2,7 +2,7 @@ public class Exception2 {
     
         public static void main (String[] args) {
             int a=10;
-            for(int i=5;i>=0;i--)
+            for(int i = 5; i >= 0; i--)
                try{
                  System.out.println(a/i);  
                }catch(ArithmeticException e){
