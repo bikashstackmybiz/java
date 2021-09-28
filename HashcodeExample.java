700ddc50dce96851125d0f572ddf82ac4e7a3bd0
Bikash Roshan <bikash.roshan@stackmybiz.com>
2021-09-28 18:24:19 +0530

    exercise of 28

---
+++ HashcodeExample.java
@@ -0,0 +1,18 @@
+public class HashcodeExample {
+    public static void main(String[] args) {
+        
+        HashcodeExample bikash =  new HashcodeExample();
+        HashcodeExample akash =  new HashcodeExample();
+        HashcodeExample siyaram =  new HashcodeExample();
+        HashcodeExample roshan =  new HashcodeExample();
+
+        System.out.println("hashcode of bikash object is  " + bikash.hashCode());  // hashcode of all the object are diffrent
+        System.out.println("hashcode of akash object is  " + akash.hashCode());
+        System.out.println("hashcode of siyaram object is  " + siyaram.hashCode());
+        System.out.println("hashcode of roshan object is  " + roshan.hashCode());
+
+
+
+        
+    }
+}
